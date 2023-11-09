89112bffcf7a7cca1bc6f2e82a8b07c304360277
Alberopinero <apinero@iesrafaelalberti.es>
2023-11-09 08:18:46 +0100

    Commit ejercicios

--- src/ejercicio7.py
+++
@@ -1,8 +0,0 @@
-persona = {}
-clave = input('¿Qué dato quieres introducir? ')
-while clave != "salir":
-    valor = input(clave + ': ')
-    persona[clave] = valor
-    print(persona)
-    otroDato = input("¿Qué otro dato quieres introducir?: ")
-    (clave)
